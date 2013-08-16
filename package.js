c6fd2429879002195010caca0a9b9cc3f10c51bb
Hubert OG <hubert@orlikarnia.com>
2013-08-16 15:17:48 +0200

    Upgrade for 0.6.5

--- package.js
+++ package.js
@@ -8,6 +8,11 @@ Package.describe({
 
 Package.on_use(function (api, where) {
 
+  if(api.export) {
+    api.use('deps', ['client', 'server']);
+    api.use('mongo-livedata', ['client', 'server']);
+  }
+
   api.add_files([
     'files/_.js',
     'files/_database.js',
